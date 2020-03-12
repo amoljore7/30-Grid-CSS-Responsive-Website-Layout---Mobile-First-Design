13824ca060c11e14bc3754ef191bbb004ab77e1f
amoljore7 <amoljore7@gmail.com>
2020-03-12 18:56:29 +0530

    Updates

---
+++ precache-manifest.69d1e76e00905b396dcbf9226c5bbe17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4819d57b28a6aeca31cd2d082989689c",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/index.html"
+  },
+  {
+    "revision": "e9e0755a9d4369b8e267",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/css/main.ae67c558.chunk.css"
+  },
+  {
+    "revision": "122b145dd9e3cb7f2071",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/js/2.718f3203.chunk.js"
+  },
+  {
+    "revision": "e9e0755a9d4369b8e267",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/js/main.d19c4a84.chunk.js"
+  },
+  {
+    "revision": "be8ad51196dea79d8f9e",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/js/runtime~main.180ccdb7.js"
+  },
+  {
+    "revision": "a9cd0e2398d5ac3dab954fb815d27227",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/media/bg.a9cd0e23.jpg"
+  },
+  {
+    "revision": "83bbd3a31380933fab7e3b691f0800a6",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/media/card1.83bbd3a3.jpg"
+  },
+  {
+    "revision": "5dcf0b4dd855d14d9a2b979d5d17f231",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/media/marketing.5dcf0b4d.jpg"
+  },
+  {
+    "revision": "aae30a22939029bd8d929bc656f3e188",
+    "url": "/30-Grid-CSS-Responsive-Website-Layout---Mobile-First-Design/static/media/mobile.aae30a22.jpg"
+  }
+]);
